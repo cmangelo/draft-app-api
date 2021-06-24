411ec48c902bbf12087c6aa7dc8c4593a3e2d877
Christian Angelo <cangloa@amazon.com>
2021-06-24 16:12:52 -0700

    Update test route to log request body to console

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,10 @@ const app = express()
 
 const PORT = 8080
 
-app.get('/test', (req: any, res: any) => res.send('hiya'));
+app.get('/test', (req: any, res: any) => {
+  console.log(JSON.stringify(req))
+  res.send('hiya')
+});
 
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT)
